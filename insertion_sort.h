d6f91b7b45d4fc1880bcc98e3d99005dab1295e0
Juaneliz <A01708389@tec.mx>
2025-10-19 20:10:11 -0600

    Update insertion_sort.h
    Corrección de insertion_sort

--- insertion_sort.h
+++ insertion_sort.h
@@ -10,15 +10,21 @@ public:
     // v = la columna a usar como criterio
     // tabla = objeto que tiene swapfila
     template <class Clima>
-    void insertionSort(vector<T> &v, Clima *tabla) {
-        for (size_t i = 1; i < v.size(); i++) {
-            int j = i;
-            while (j > 0 && v[j] < v[j - 1]) {
-                tabla->swapfila(j, j - 1); // aquí usamos el swapfila del objeto
-                j--;
-            }
-        }
-    }
+    void insertionSort(vector<T> &v, Clima *tabla);
 };
 
+
+template <class T>
+template <class Clima>
+void Insertion_sorts<T>:: insertionSort(vector<T> &v, Clima *tabla) {
+    for (size_t i = 1; i < v.size(); i++) {
+        int j = i;
+        while (j > 0 && v[j] < v[j - 1]) {
+            tabla->swapfila(j, j - 1); // aquí usamos el swapfila del objeto
+            j--;
+        }
+    }
+}
 #endif
+
+
